180745f8cc1b7c7721490d4b60370718286aa610
Nahid <j.h.nahid77@gmail.com>
2025-03-17 23:41:31 +0600

    add new url

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
     'https://portal.ai-tuningfiles.com',
     'https://portal.tuningfile-server.com',
     'https://portal.remap.no',
-    'http://ecufiles-austria.com',
+    'https://ecufiles-austria.com',
     'https://www.ecufiles-austria.com',
   ];
 
