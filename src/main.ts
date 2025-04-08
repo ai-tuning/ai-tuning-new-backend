10b7967a5f7e6970cedc844bf9a6407cbb3d943f
Nahid <j.h.nahid77@gmail.com>
2025-04-08 19:35:10 +0600

    add new url

--- src/main.ts
+++ src/main.ts
@@ -15,63 +15,65 @@ import { AllExceptionFilter, I18nInterceptor, ErrorFormatter } from './features/
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const config = app.get(ConfigService);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    const config = app.get(ConfigService);
 
-  const origins = [
-    'https://admin.ai-tuningfiles.com',
-    'https://portal.ai-tuningfiles.com',
-    'https://portal.tuningfile-server.com',
-    'https://portal.remap.no',
-    'https://admin.remap.no',
-    'https://ecufiles-austria.com',
-    'https://www.ecufiles-austria.com',
-    'https://admin.ecufiles-austria.com',
-    'https://dyno-files.org',
-  ];
+    const origins = [
+        'https://admin.ai-tuningfiles.com',
+        'https://portal.ai-tuningfiles.com',
+        'https://portal.tuningfile-server.com',
+        'https://portal.remap.no',
+        'https://admin.remap.no',
+        'https://ecufiles-austria.com',
+        'https://www.ecufiles-austria.com',
+        'https://admin.ecufiles-austria.com',
+        'https://dyno-files.org',
+        'https://portal.kap-tuningfiles.com',
+        'https://admin.kap-tuningfiles.com',
+    ];
 
-  if (process.env.NODE_ENV === 'development') {
-    origins.push('http://localhost:3000');
-    origins.push('http://localhost:4000');
-  }
+    if (process.env.NODE_ENV === 'development') {
+        origins.push('http://localhost:3000');
+        origins.push('http://localhost:4000');
+    }
 
-  app.enableCors({
-    origin: origins,
-    credentials: true,
-  });
-  app.setGlobalPrefix('api/v1'); //route prefix
-  app.useGlobalPipes(
-    new ValidationPipe({
-      stopAtFirstError: true,
-      transform: true,
-      enableDebugMessages: true,
-      forbidNonWhitelisted: true,
-      validationError: {
-        target: false,
-        value: false,
-      },
-      validateCustomDecorators: true,
-      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+    app.enableCors({
+        origin: origins,
+        credentials: true,
+    });
+    app.setGlobalPrefix('api/v1'); //route prefix
+    app.useGlobalPipes(
+        new ValidationPipe({
+            stopAtFirstError: true,
+            transform: true,
+            enableDebugMessages: true,
+            forbidNonWhitelisted: true,
+            validationError: {
+                target: false,
+                value: false,
+            },
+            validateCustomDecorators: true,
+            errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
 
-      exceptionFactory: (errors) => {
-        const errorObject = ErrorFormatter(errors); //convert validation error message
-        throw new BadRequestException(errorObject);
-      },
-    }),
-  );
+            exceptionFactory: (errors) => {
+                const errorObject = ErrorFormatter(errors); //convert validation error message
+                throw new BadRequestException(errorObject);
+            },
+        }),
+    );
 
-  const yamlContent = fs.readFileSync(path.join(process.cwd(), 'docs.yaml'), 'utf-8');
-  const document: OpenAPIObject = yaml.load(yamlContent) as OpenAPIObject;
-  SwaggerModule.setup('/api-docs', app, document);
-  const httpAdapter = app.get(HttpAdapterHost);
-  app.set('trust proxy', 'loopback'); // Trust requests from the loopback address
-  app.use(compression());
-  app.use(helmet());
-  app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
-  app.use(cookieParser(config.get<string>('cookie_secret')));
-  app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
-  app.useGlobalInterceptors(new I18nInterceptor());
-  await app.listen(2600);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+    const yamlContent = fs.readFileSync(path.join(process.cwd(), 'docs.yaml'), 'utf-8');
+    const document: OpenAPIObject = yaml.load(yamlContent) as OpenAPIObject;
+    SwaggerModule.setup('/api-docs', app, document);
+    const httpAdapter = app.get(HttpAdapterHost);
+    app.set('trust proxy', 'loopback'); // Trust requests from the loopback address
+    app.use(compression());
+    app.use(helmet());
+    app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+    app.use(cookieParser(config.get<string>('cookie_secret')));
+    app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
+    app.useGlobalInterceptors(new I18nInterceptor());
+    await app.listen(2600);
+    console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
