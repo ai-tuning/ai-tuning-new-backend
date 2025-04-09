439541a15fac2aae346e9aabe9a80ee3898a7254
Nahid <j.h.nahid77@gmail.com>
2025-04-09 21:16:28 +0600

    fix 5 digit verification code problem

--- src/features/verification-mail/verification-mail.service.ts
+++ src/features/verification-mail/verification-mail.service.ts
@@ -7,65 +7,69 @@ import { collectionsName, VerificationEmailEnum } from 'src/features/constant';
 
 @Injectable()
 export class VerificationMailService {
-  constructor(
-    @InjectModel(collectionsName.verificationMail) private readonly verificationEmailModel: Model<VerificationEmail>,
-  ) {}
+    constructor(
+        @InjectModel(collectionsName.verificationMail)
+        private readonly verificationEmailModel: Model<VerificationEmail>,
+    ) {}
 
-  private generateRandomCode(): string {
-    //generate 6 digit random code
-    return Math.floor(Math.random() * 1000000).toString();
-  }
+    private generateRandomCode(): string {
+        // Generate 6-digit random code
+        const code = Math.floor(Math.random() * 1000000).toString();
 
-  async createVerificationEmail(
-    email: string,
-    verificationType: VerificationEmailEnum,
-    session?: ClientSession,
-  ): Promise<VerificationEmail> {
-    const code = this.generateRandomCode();
-    const verificationEmail = new this.verificationEmailModel({
-      email,
-      verificationType: verificationType,
-      code,
-      duration: 5,
-    });
-    if (session) {
-      return verificationEmail.save({ session });
-    }
-    return verificationEmail.save();
-  }
-
-  async getVerificationEmail(email: string, verificationType: VerificationEmailEnum): Promise<VerificationEmail> {
-    return this.verificationEmailModel.findOne({ email, verificationType });
-  }
-
-  async verifyEmail(
-    email: string,
-    code: string,
-    verificationType: VerificationEmailEnum,
-    session: ClientSession,
-  ): Promise<boolean> {
-    const data = await this.verificationEmailModel
-      .findOne({ email, code, verificationType, isUsed: false })
-      .sort({ createdAt: -1 });
-    if (!data) {
-      throw new BadRequestException('Invalid verification code');
+        // Pad with leading zeros if the code is less than 6 digits
+        return code.padStart(6, '0');
     }
 
-    const now = new Date();
-    const difference = differenceInMinutes(now, data.createdAt);
-    if (difference > data.duration) {
-      this.verificationEmailModel.deleteMany({ email, code }).exec();
-      throw new BadRequestException('Verification code expired');
+    async createVerificationEmail(
+        email: string,
+        verificationType: VerificationEmailEnum,
+        session?: ClientSession,
+    ): Promise<VerificationEmail> {
+        const code = this.generateRandomCode();
+        const verificationEmail = new this.verificationEmailModel({
+            email,
+            verificationType: verificationType,
+            code,
+            duration: 5,
+        });
+        if (session) {
+            return verificationEmail.save({ session });
+        }
+        return verificationEmail.save();
     }
 
-    data.isUsed = true;
-
-    if (session) {
-      await data.save({ session });
-    } else {
-      await data.save();
+    async getVerificationEmail(email: string, verificationType: VerificationEmailEnum): Promise<VerificationEmail> {
+        return this.verificationEmailModel.findOne({ email, verificationType });
     }
 
-    return true;
-  }
+    async verifyEmail(
+        email: string,
+        code: string,
+        verificationType: VerificationEmailEnum,
+        session: ClientSession,
+    ): Promise<boolean> {
+        const data = await this.verificationEmailModel
+            .findOne({ email, code, verificationType, isUsed: false })
+            .sort({ createdAt: -1 });
+        if (!data) {
+            throw new BadRequestException('Invalid verification code');
+        }
+
+        const now = new Date();
+        const difference = differenceInMinutes(now, data.createdAt);
+        if (difference > data.duration) {
+            this.verificationEmailModel.deleteMany({ email, code }).exec();
+            throw new BadRequestException('Verification code expired');
+        }
+
+        data.isUsed = true;
+
+        if (session) {
+            await data.save({ session });
+        } else {
+            await data.save();
+        }
+
+        return true;
+    }
 }
